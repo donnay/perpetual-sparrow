b61c12da74b5b28d3773a7b04b66098c7426fe49
stackbit-projects <projects@stackbit.com>
2020-07-30 00:36:33 +0000

    Update configuration

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Stackbit Exto Theme original README is located [here](./README.theme.md).
 
 1. Run the following command to fetch additional site contents from Stackbit if needed:
 
-        npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
+        npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5f2215905fdcd8001c54eb0e
 
 1. Start a local development server:
 
--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,12 +4,12 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5f2215905fdcd8001c54eb0e/webhook/build/pull > /dev/null
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5f2215905fdcd8001c54eb0e
 fi
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5f2215905fdcd8001c54eb0e/webhook/build/ssgbuild > /dev/null
 gatsby build
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://api.stackbit.com/project/5f2215905fdcd8001c54eb0e/webhook/build/publish > /dev/null
